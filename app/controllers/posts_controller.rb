a19a7dd86e3221afb926e4a5f0e63b9c3f0e9dcd
ngso05 <nkxxo05@gmail.com>
2021-09-19 09:59:20 +0000

    update 投稿機能更新

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,15 +15,25 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def show
+    @post = Post.find(params[:id])
+    @user = @post.user
+    # binding.irb
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path(@post)
   end
 
   private
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ab38c9c021282a42e21f722f0cca4d944e7da7b279f6c07b96d8c77777dae468456717a833f4043f7aa734170a5b2a80b3f94ea9800cd6c5a3e6a40ffac1b53e'
